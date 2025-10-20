2b5e7ea2badd92ebea254cee3d0424638f8ce2ba
Simon Hawkenson <7358924+sihawken@users.noreply.github.com>
2025-10-19 23:00:02 -0230

    remove imports

--- assets/js/twindconfig.js
+++ assets/js/twindconfig.js
@@ -1,6 +1,3 @@
-import { install, defineConfig } from 'https://cdn.twind.style/core';
-import presetTailwind from 'https://cdn.twind.style/preset-tailwind';
-
 const config = defineConfig({
   presets: [presetTailwind()],
   prefix: 'tw-',
