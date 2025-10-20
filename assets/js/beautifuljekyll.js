654afcbe1677cc551bc848a9a36e90c56ad0d0f7
Simon Hawkenson <7358924+sihawken@users.noreply.github.com>
2025-10-20 01:07:06 -0230

    try what Gemini suggested

--- assets/js/beautifuljekyll.js
+++ assets/js/beautifuljekyll.js
@@ -1,4 +1,4 @@
-// Dean Attali / Beautiful Jekyll 2023
+// assets/js/beautifuljekyll.js
 
 let BeautifulJekyllJS = {
 
@@ -6,44 +6,10 @@ let BeautifulJekyllJS = {
   numImgs : null,
 
   init : function() {
-    setTimeout(BeautifulJekyllJS.initNavbar, 10);
-
-    // Shorten the navbar after scrolling a little bit down
-    $(window).scroll(function() {
-        if ($(".navbar").offset().top > 50) {
-            $(".navbar").addClass("top-nav-short");
-        } else {
-            $(".navbar").removeClass("top-nav-short");
-        }
-    });
-
-    // On mobile, hide the avatar when expanding the navbar menu
-    $('#main-navbar').on('show.bs.collapse', function () {
-      $(".navbar").addClass("top-nav-expanded");
-    });
-    $('#main-navbar').on('hidden.bs.collapse', function () {
-      $(".navbar").removeClass("top-nav-expanded");
-    });
-
     // show the big header image
-    BeautifulJekyllJS.initImgs();
+    BeautifulJekyllJS.initImms();
 
     BeautifulJekyllJS.initSearch();
-  },
-
-  initNavbar : function() {
-    // Set the navbar-dark/light class based on its background color
-    const rgb = $('.navbar').css("background-color").replace(/[^\d,]/g,'').split(",");
-    const brightness = Math.round(( // http://www.w3.org/TR/AERT#color-contrast
-      parseInt(rgb[0]) * 299 +
-      parseInt(rgb[1]) * 587 +
-      parseInt(rgb[2]) * 114
-    ) / 1000);
-    if (brightness <= 125) {
-      $(".navbar").removeClass("navbar-light").addClass("navbar-dark");
-    } else {
-      $(".navbar").removeClass("navbar-dark").addClass("navbar-light");
-    }
   },
 
   initImgs : function() {
